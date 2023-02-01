7b182d49793f299574bce8455191bb04f2232bee
赵世玮 <zhaoshiwei@corp.netease.com>
2023-02-01 10:03:50 +0800

    init

--- code/model.py
+++ code/model.py
@@ -10,59 +10,10 @@ import torchmetrics
 from sklearn.model_selection import train_test_split
 from torch import nn
 from torch.utils.data import Dataset, DataLoader
+from utils import EarlyStopping
 
 
-class EarlyStopping:
-    """Early stops the training if validation loss doesn't improve after a given patience."""
 
-    def __init__(self, patience=7, verbose=False, delta=0, path='checkpoint.pt', trace_func=print):
-        """
-        Args:
-            patience (int): How long to wait after last time validation loss improved.
-                            Default: 7
-            verbose (bool): If True, prints a message for each validation loss improvement.
-                            Default: False
-            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
-                            Default: 0
-            path (str): Path for the checkpoint to be saved to.
-                            Default: 'checkpoint.pt'
-            trace_func (function): trace print function.
-                            Default: print
-        """
-        self.patience = patience
-        self.verbose = verbose
-        self.counter = 0
-        self.best_score = None
-        self.early_stop = False
-        self.val_loss_min = np.Inf
-        self.delta = delta
-        self.path = path
-        self.trace_func = trace_func
-
-    def __call__(self, val_loss, model):
-
-        score = -val_loss
-
-        if self.best_score is None:
-            self.best_score = score
-            self.save_checkpoint(val_loss, model)
-        elif score < self.best_score + self.delta:
-            self.counter += 1
-            self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
-            if self.counter >= self.patience:
-                self.early_stop = True
-        else:
-            self.best_score = score
-            self.save_checkpoint(val_loss, model)
-            self.counter = 0
-
-    def save_checkpoint(self, val_loss, model):
-        '''Saves model when validation loss decrease.'''
-        if self.verbose:
-            self.trace_func(
-                f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
-        torch.save(model.state_dict(), self.path)
-        self.val_loss_min = val_loss
 
 
 class TSCDataset(Dataset):
@@ -260,7 +211,7 @@ class LSDSModel:
             acc_metric = torchmetrics.Accuracy(num_classes=self.num_classes).to(self.device)
 
             self.net.train()
-            for step, (x, y) in enumerate(train_loader):
+            for _, (x, y) in enumerate(train_loader):
                 x = x.float().to(self.device)
                 y = y.long().to(self.device)
                 y_, _, _ = self.net(x)
@@ -278,7 +229,7 @@ class LSDSModel:
 
             self.net.eval()
             with torch.no_grad():
-                for step, (x, y) in enumerate(val_loader):
+                for _, (x, y) in enumerate(val_loader):
                     x = x.float().to(self.device)
                     y = y.long().to(self.device)
                     y_, _, _ = self.net(x)
@@ -321,7 +272,7 @@ class LSDSModel:
         p = []
         self.net.eval()
         with torch.no_grad():
-            for step, (x, y) in enumerate(test_loader):
+            for _, (x, y) in enumerate(test_loader):
                 x = x.float().to(self.device)
                 y = y.long().to(self.device)
                 y_, _, p_ = self.net(x)
